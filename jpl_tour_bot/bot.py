98dbf5544273113d4d3f024fc52859792fa1cf98
dpet23 <dpet23@users.noreply.github.com>
2024-02-28 20:41:44 +1100

    Updated notification to show the details of available tours as a table

--- jpl_tour_bot/bot.py
+++ jpl_tour_bot/bot.py
@@ -6,11 +6,13 @@ import logging
 import random
 import time
 from textwrap import indent
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, NamedTuple
 
+from markdown_strings import code_block  # type: ignore[import-untyped]
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from tabulate import tabulate
 
 from jpl_tour_bot import SCREENSHOT_PATH, URL_JPL_TOUR, WAIT_TIME_LIMITS, Args
 from jpl_tour_bot.browser import ChromeWebDriver
@@ -23,7 +25,18 @@ if TYPE_CHECKING:
 LOGGER = logging.getLogger(__name__)
 
 
-def run_bot(args: Args, state: State) -> list[str]:
+class Notification(NamedTuple):
+    """An important state change to report as a notification."""
+
+    title: str
+    content: str
+
+    def __str__(self) -> str:
+        """Represent the notification as a string."""
+        return f"{self.title}\n{indent(self.content, '\t')}"
+
+
+def run_bot(args: Args, state: State) -> list[Notification]:
     """
     Scrape the NASA JPL tours webpage.
 
@@ -53,7 +66,7 @@ def run_bot(args: Args, state: State) -> list[str]:
         browser.shut_down()
 
 
-def _scrape_tour(browser: ChromeWebDriver, state: State) -> list[str]:
+def _scrape_tour(browser: ChromeWebDriver, state: State) -> list[Notification]:
     """
     Find whether any NASA JPL tours are available.
 
@@ -66,16 +79,16 @@ def _scrape_tour(browser: ChromeWebDriver, state: State) -> list[str]:
 
     time.sleep(5)
 
-    notification_messages = []
+    notification_messages: list[Notification] = []
 
     # Search for the date of the next tour release.
     next_tour_msg = _get_next_tour_release_date(browser)
 
     # Check if the next tour release date has changed.
     if next_tour_msg != state.NEXT_TOUR_MSG:
-        notification_msg = f'Next tour message has changed\n\t{next_tour_msg}'
-        notification_messages.append(notification_msg)
-        LOGGER.info(notification_msg)
+        notification = Notification('Next tour message has changed', next_tour_msg)
+        notification_messages.append(notification)
+        LOGGER.info(notification)
 
         state.NEXT_TOUR_MSG = next_tour_msg
 
@@ -83,15 +96,16 @@ def _scrape_tour(browser: ChromeWebDriver, state: State) -> list[str]:
 
     # Search for available tours.
     _submit_tour_search_form(browser, tour_type='Visitor Day Tour', tour_size=1)
-    tour_available_msg = _get_tour_availability_after_search(browser)
+    tour_availability_notifications = _get_tour_availability_after_search(browser)
 
     # Check if the tour availability has changed.
-    if tour_available_msg != state.TOUR_AVAILABLE:
-        notification_msg = f"Tour availability has changed\n{indent(tour_available_msg, '\t')}"
-        notification_messages.append(notification_msg)
-        LOGGER.info(notification_msg)
+    tour_availability_msg = '\n'.join(notification.content for notification in tour_availability_notifications)
+    if tour_availability_msg != state.TOUR_AVAILABLE:
+        for notification in tour_availability_notifications:
+            notification_messages.append(notification)
+            LOGGER.info(notification)
 
-        state.TOUR_AVAILABLE = tour_available_msg
+        state.TOUR_AVAILABLE = tour_availability_msg
 
     time.sleep(5)
 
@@ -159,12 +173,12 @@ def _submit_tour_search_form(browser: ChromeWebDriver, *, tour_type: str, tour_s
         raise RuntimeError("Can't click on %s", submit_form_button.get_attribute('outerHTML')) from None
 
 
-def _get_tour_availability_after_search(browser: ChromeWebDriver) -> str:
+def _get_tour_availability_after_search(browser: ChromeWebDriver) -> list[Notification]:
     """
     After searching for tours by submitting a form, check if there's any tours available.
 
     :param browser: The open browser instance.
-    :return: The availability of the next tours, from the JPL website.
+    :return: The availability of the next tours, from the JPL website, as a list of notifications to report.
     """
     LOGGER.info('Waiting for the tour search to load')
     browser.wait_until_visible(By.XPATH, "//*[@id='primary_column']/div/table")
@@ -180,24 +194,26 @@ def _get_tour_availability_after_search(browser: ChromeWebDriver) -> str:
         # Use a custom exception handler to suppress the error message.
         error_msg_element = None
 
+    notification_title_new_availability = 'Tour availability has changed'
+
     if error_msg_element:
-        return error_msg_element.text.strip()
+        return [Notification(notification_title_new_availability, error_msg_element.text.strip())]
 
     LOGGER.info('Trying to find the number of available tours')
     tour_availability_msg = browser.find_by_class('tour_count')
     if not tour_availability_msg:
-        return 'No tours found.'
+        return [Notification(notification_title_new_availability, 'No tours found.')]
 
-    tour_availability_notification_msg = tour_availability_msg.text.strip()
+    notifications = [Notification(notification_title_new_availability, tour_availability_msg.text.strip())]
 
     LOGGER.info('Parsing the table of available tours')
     available_tours_table = browser.find_by_class('available_tours')
-    available_tour_details = ''
     if available_tours_table:
         available_tour_details = _parse_available_tours_table(browser, available_tours_table)
+        notifications.append(Notification('Tour details', available_tour_details))
 
     browser.save_screenshot_full_page(str(SCREENSHOT_PATH.absolute()))
-    return tour_availability_notification_msg + available_tour_details
+    return notifications
 
 
 def _parse_available_tours_table(browser: ChromeWebDriver, available_tours_table: WebElement) -> str:
@@ -206,27 +222,28 @@ def _parse_available_tours_table(browser: ChromeWebDriver, available_tours_table
 
     :param browser: The open browser instance.
     :param available_tours_table: Web element representing the table of available tours.
-    :return: The details of available tours, as a string.
+    :return: The details of available tours, as a multiline string representing a table.
     """
     table_rows = browser.find_by_tag('tr', available_tours_table, multiple=True)
 
     # Ignore the buttons for making a reservation, only interested in the tour details.
     str_to_ignore = 'Reserve'
 
-    # The top row lists the headings, record these.
-    table_headings = []
-    for heading_col in browser.find_by_tag('td', table_rows[0], multiple=True):
-        if str_to_ignore not in heading_col.text:
-            table_headings.append(heading_col.text.strip())
+    # Extract the table content.
+    table_header = []
+    table_data_rows = []
 
-    available_tours = ''
+    for i, table_row in enumerate(table_rows):
+        row_content = [
+            col.text.strip()
+            for col in browser.find_by_tag('td', table_row, multiple=True)
+            if str_to_ignore not in col.text
+        ]
 
-    # Convert the table content into dicts.
-    for content_row in table_rows[1:]:
-        tour_details = {}
-        for i, content_col in enumerate(browser.find_by_tag('td', content_row, multiple=True)):
-            if str_to_ignore not in content_col.text:
-                tour_details[table_headings[i]] = content_col.text.strip()
-        available_tours += f'\n{tour_details}'
+        if i == 0:
+            # The top row lists the headings.
+            table_header = row_content
+        else:
+            table_data_rows.append(row_content)
 
-    return available_tours
+    return code_block(tabulate(tabular_data=table_data_rows, headers=table_header, tablefmt='psql'), language='text')
